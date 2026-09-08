7cac08944a57d6a1f6d436f03b260375d6286c23
sh97b <sh097.b@gmail.com>
2026-09-08 10:14:49 +0900

    chore: 프로젝트 초기 설정 및 환경 구성

---
+++ src/main/java/com/followup/FollowupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.followup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/followup/FollowupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.followup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
