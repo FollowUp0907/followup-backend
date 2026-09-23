e990241dbc011628aa105c34266e26fe683cf14d
sh97b <sh097.b@gmail.com>
2026-09-23 10:55:38 +0900

    fix: 예상치 못한 예외를 로그로 남기도록 긴급 수정

--- src/main/java/com/followup/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/followup/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.followup.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -50,6 +52,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unhandled exception", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
     }
